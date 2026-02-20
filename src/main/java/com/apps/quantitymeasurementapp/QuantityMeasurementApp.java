de1dda1f71ab5943ae64495bfc1911c5aba1bc79
Suraj Kumar <suraj1122003@gmail.com>
2026-02-20 15:11:44 +0530

    [suraj] : initial setup added

---
+++ src/main/java/com/apps/quantitymeasurementapp/QuantityMeasurementApp.java
@@ -0,0 +1,10 @@
+package com.apps.quantitymeasurementapp;
+
+public class QuantityMeasurementApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/apps/QuantityMeasurementApp/QuantityMeasurementAppTest.java
@@ -0,0 +1,5 @@
+package com.apps.QuantityMeasurementApp;
+
+public class QuantityMeasurementAppTest {
+
+}
