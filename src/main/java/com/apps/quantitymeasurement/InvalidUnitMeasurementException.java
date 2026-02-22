51b63ee0047306eb4339a7f9b8db96002c74cd8b
Suraj Kumar <suraj1122003@gmail.com>
2026-02-22 22:35:17 +0530

    [Suraj]: Added InvalidUnitMeasurementException class file

---
+++ src/main/java/com/apps/quantitymeasurement/InvalidUnitMeasurementException.java
@@ -0,0 +1,8 @@
+package com.apps.quantitymeasurement;
+
+public class InvalidUnitMeasurementException extends Exception{
+
+	public InvalidUnitMeasurementException(String msg) {
+		super(msg);
+	}
+}
