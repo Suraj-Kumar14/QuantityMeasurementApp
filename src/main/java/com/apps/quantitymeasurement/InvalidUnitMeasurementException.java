f56e3f1df649c3aa6a496de3b860e9da9fb98cea
Suraj Kumar <suraj1122003@gmail.com>
2026-02-24 22:07:43 +0530

    [Suraj]: Remove unwanted file

--- src/main/java/com/apps/quantitymeasurement/InvalidUnitMeasurementException.java
+++
@@ -1,8 +0,0 @@
-package com.apps.quantitymeasurement;
-
-public class InvalidUnitMeasurementException extends Exception{
-
-	public InvalidUnitMeasurementException(String msg) {
-		super(msg);
-	}
-}
