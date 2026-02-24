aa093db3af361625f2e9fb01bd0f2e23770be187
Suraj Kumar <suraj1122003@gmail.com>
2026-02-24 10:22:11 +0530

    [Suraj]: Added WeightUnit enum file

---
+++ src/main/java/com/apps/quantitymeasurement/WeightUnit.java
@@ -0,0 +1,32 @@
+package com.apps.quantitymeasurement;
+
+public enum WeightUnit {
+
+	//Conversion factor to the base unit (grams)
+	MILLIGRAM(0.001),
+	GRAM(1.0),
+	KILOGRAM(1000.0),
+	POUND(453.592),
+	TONNE(1_000_000.0);
+	
+	//Conversion factor to the base unit (grams)
+	private final double conversionFactor;
+	
+	//Constructor to initialize the conversion factor
+	WeightUnit(double conversionFactor){
+		this.conversionFactor=conversionFactor;
+	}
+	
+	public double getConversionFactor() {
+		return conversionFactor;
+	}
+	
+	public double convertToBaseUnit(double value) {
+		return value*this.getConversionFactor();
+	}
+	
+	public double convertFromBaseUnit(double baseValue) {
+		return (baseValue/this.getConversionFactor()*100.0)/100.0;
+	}
+	
+}
