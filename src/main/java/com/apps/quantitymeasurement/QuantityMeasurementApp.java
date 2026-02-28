f2bba9a69ce5eebc05c370e77cd0fa1911657edc
Suraj Kumar <suraj1122003@gmail.com>
2026-02-28 10:31:39 +0530

    [Suraj]: Update QuantityMeasurementApp class file

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -42,6 +42,34 @@ public class QuantityMeasurementApp {
 		return quantity1.add(quantity2, targetUnit);
 	}			
 	
+public static <U extends IMeasurable> Quantity<U> demonstrateSubtraction(Quantity<U> quantity1, Quantity<U> quantity2){
+		
+		if(quantity1==null || quantity2==null || quantity1.getUnit().getClass()!=quantity2.getUnit().getClass()) {
+			throw new IllegalArgumentException("Invalid quantity!");
+		}
+		
+		return quantity1.subtract(quantity2);
+	}
+	
+	public static <U extends IMeasurable> Quantity<U> demonstrateSubtraction(Quantity<U> quantity1, Quantity<U> quantity2, U targetUnit){
+		if(quantity1==null || quantity2==null || quantity1.getUnit().getClass()!=quantity2.getUnit().getClass()) {
+			throw new IllegalArgumentException("Invalid quantity");
+		}
+		
+		if(targetUnit==null) {
+			throw new IllegalArgumentException("Invalid targetUnit");
+		}
+		
+		return quantity1.subtract(quantity2, targetUnit);
+	}
+	
+	public static <U extends IMeasurable> double demonstrateDivision(Quantity<U> quantity1, Quantity<U> quantity2) {
+		if(quantity1==null || quantity2==null || quantity1.getUnit().getClass()!=quantity2.getUnit().getClass()) {
+			throw new IllegalArgumentException("Invalid quantity!");
+		}
+		
+		return quantity1.divide(quantity2);
+	}
 	
 	public static void main(String[] args) {
 		// Demonstration equality between the two quantities
@@ -70,6 +98,20 @@ public class QuantityMeasurementApp {
 		WeightUnit.GRAM);
 		System.out.println("Sum Weight in Grams: " + sumWeightInGrams.getValue() + " " +
 		sumWeightInGrams.getUnit());
+		
+		    System.out.println("Subtraction of Feet and Inches : "+demonstrateSubtraction(new Quantity<LengthUnit>(10.0,LengthUnit.FEET),new Quantity<LengthUnit>(6.0,LengthUnit.INCHES)));
+	        
+	        System.out.println("Subtraction of Kg and Gram : "+demonstrateSubtraction(new Quantity<WeightUnit>(10.0,WeightUnit.KILOGRAM), new Quantity<WeightUnit>(5000.0, WeightUnit.GRAM)));
+	        
+	        System.out.println("Subtraction of Litre and mililitre : "+demonstrateSubtraction(new Quantity<VolumeUnit>(5.0, VolumeUnit.LITRE), new Quantity<VolumeUnit>(500.0,VolumeUnit.MILLILITRE)));
+	        
+	        System.out.println("Subtraction of feet with inche to inche : "+demonstrateSubtraction(new Quantity<LengthUnit>(10.0, LengthUnit.FEET),new Quantity<LengthUnit>(6.0,LengthUnit.INCHES),LengthUnit.INCHES));
+	        
+	        System.out.println("Subtraction of kg with gram to gram : "+demonstrateSubtraction(new Quantity<WeightUnit>(10.0,WeightUnit.KILOGRAM),new Quantity<WeightUnit>(5000.0,WeightUnit.GRAM),WeightUnit.GRAM));
+	        
+	        System.out.println("Division of Feet with feet : "+demonstrateDivision(new Quantity<LengthUnit>(10.0, LengthUnit.FEET), new Quantity<LengthUnit>(2.0, LengthUnit.FEET)));
+	       
+	        System.out.println("Division of inche with feet : "+demonstrateDivision(new Quantity<LengthUnit>(24.0,LengthUnit.INCHES),new Quantity<LengthUnit>(2.0,LengthUnit.FEET)));
 	}
 	
 }
