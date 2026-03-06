722d317b0d5ef02bd658fab71e92368e6d968e4d
Suraj Kumar <suraj1122003@gmail.com>
2026-03-06 12:51:37 +0530

    [Suraj]: Update IMeasurable interface

--- src/main/java/com/apps/quantitymeasurement/IMeasurable.java
+++ src/main/java/com/apps/quantitymeasurement/IMeasurable.java
@@ -1,8 +1,26 @@
 package com.apps.quantitymeasurement;
 
+@FunctionalInterface
+interface SupportsArithmetic{
+    boolean isSupported();
+}
+
 public interface IMeasurable {
-	double getConversionFactor();
-	double convertToBaseUnit(double value);
-	double convertFromBaseUnit(double baseValue);
-	String getUnitName();
+
+    double convertToBaseUnit(double value);
+    double convertFromBaseUnit(double baseValue);
+    String getUnitName();
+
+    double getConversionFactor();
+
+    // Default lambda → all units support arithmetic
+    SupportsArithmetic supportsArithmetic = () -> true;
+
+    default boolean supportsArithmetic() {
+        return supportsArithmetic.isSupported();
+    }
+
+    // Validate arithmetic support
+    default void validateOperationSupport(String operation) {
+    }
 }
