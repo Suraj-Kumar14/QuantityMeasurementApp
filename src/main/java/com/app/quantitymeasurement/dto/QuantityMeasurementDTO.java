599dbb52379b7b125c08afe30759326a7b29101d
Suraj Kumar <suraj1122003@gmail.com>
2026-03-30 14:13:38 +0530

    [Suraj]: Added QuantityMeasurementDTO class file

--- src/main/java/com/app/quantitymeasurement/dto/QuantityMeasurementDTO.java
+++ src/main/java/com/app/quantitymeasurement/dto/QuantityMeasurementDTO.java
@@ -1,8 +1,9 @@
-package com.app.quantitymeasurement.model;
+package com.app.quantitymeasurement.dto;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import com.app.quantitymeasurement.entity.QuantityMeasurementEntity;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
